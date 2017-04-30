95080eeca83ef6107c3a67dd39fc6b3b981436d4
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2017-04-30 18:48:02 -0400

    Adding old code

--- twitter_stream_consumer.py
+++ twitter_stream_consumer.py
@@ -66,39 +66,27 @@ tweet_text = tweet.map(get_tweet_text)
 tweet_text_with_sentiment = tweet_text.map(lambda text: (text, TextBlob(text).sentiment.polarity) )
 tweet_sentiment = tweet_text_with_sentiment.map(lambda (k,v): (v, 1))
 
-# Aggregate each Tweet's sentiment and compute the mean
+# Aggregate each Tweet's sentiment
 tweet_sentiment_agg = tweet_sentiment.reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]))
-tweet_sentiment_mean = tweet_sentiment_agg.map(lambda (sent_sum, count): float(sent_sum) / count)
+
+# Use accumulators to keep a running total of the sum and average sentiment
+tweet_sentiment_sum = streamContext.sparkContext.accumulator(0)
+tweet_sentiment_count  = streamContext.sparkContext.accumulator(0)
+
+def increment_accumulators(sent_sum, count):
+    tweet_sentiment_sum.add(sent_sum)
+    tweet_sentiment_count.add(count)
+    return None
+
+tweet_sentiment_agg.foreachRDD(lambda rdd: rdd.foreach(lambda (sent_sum, count): increment_accumulators(sent_sum, count) ) )
+print(tweet_sentiment_sum.value, tweet_sentiment_count.value)#, float(tweet_sentiment_sum.value) / tweet_sentiment_count.value
+tweet_sentiment_sum.value
+
+#tweet_sentiment_mean = tweet_sentiment_agg.map(lambda (sent_sum, count): (float(sent_sum) / count, count))
 
 # Print results
-tweet_sentiment_mean.pprint()
+#tweet_sentiment_mean.pprint()
 
 # Start Spark stream
 streamContext.start()
 streamContext.awaitTermination()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Graveyard
-'''
-tweet_rows = tweet_text_with_sentiment.map(lambda (text, sentiment): Row(text=text, sentiment=sentiment))
-tweet_df = sqlContext.createDataFrame(tweet_rows)
-tweet_df.registerTemplate("tweets")
-
-average_sentiment = sqlContext.sql("SELECT SUM(sentiment) / COUNT(sentiment) FROM tweets")
-average_sentiment.show()
-'''
